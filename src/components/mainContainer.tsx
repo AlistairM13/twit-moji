1f48eaca9f91497c7835e66f60e41259e865690d
Alistair <alistairm2022@gmail.com>
2023-06-25 16:53:03 +0530

    overflow css

--- src/components/mainContainer.tsx
+++ src/components/mainContainer.tsx
@@ -2,8 +2,8 @@ import type { PropsWithChildren } from "react";
 
 export const PageLayout = (props: PropsWithChildren) => {
     return (
-        <main className="h-screen overflow-none  flex justify-center">
-            <div className="w-full md:max-w-2xl border-x border-slate-400 ">
+        <main className="h-screen overflow-none flex justify-center">
+            <div className="w-full md:max-w-2xl border-x border-slate-400 overflow-y-scroll">
                     {props.children}
             </div>
         </main>
